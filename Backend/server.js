09f9d44cc20879807c89bc839ac02882173b6c5b
Henry Omino <henryedwin92@gmail.com>
2026-06-18 13:55:06 +0300

    portfolio setup

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
